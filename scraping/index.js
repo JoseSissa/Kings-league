2fec0de858d24a9d8e7fdffd3300b2c9f1d254d4
Jose Sissa <jsissame3484@universidadean.edu.co>
2023-01-08 17:16:12 -0500

    Get info from web with cheerio (scraping)

--- scraping/index.js
+++ scraping/index.js
@@ -4,40 +4,40 @@ const URLS = {
     leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
 }
 
-const scrape = async (url) => {
+async function scrape (url) {
     const res = await fetch(url)
     const html = await res.text()
     return cheerio.load(html)
 }
 
-const $ = scrape(URLS.leaderboard)
+async function getLeaderBoard () {
+    const $ = await scrape(URLS.leaderboard)
+    const $rows = $('table tbody tr')
 
-$('table tbody tr').each((index, elem) => {
-    const rawTeam = $(elem).find('.fs-table-text_3').text()
-    const rawVistories = $(elem).find('.fs-table-text_4').text()
-    const rawLoses = $(elem).find('.fs-table-text_5').text()
-    const rawScored = $(elem).find('.fs-table-text_6').text()
-    const rawConceded = $(elem).find('.fs-table-text_7').text()
-    const rawCardsYellow = $(elem).find('.fs-table-text_8').text()
-    const rawCardsRed = $(elem).find('.fs-table-text_9').text()
+    const LEADERBOARD_SELECTORS = {
+        team: '.fs-table-text_3',
+        vistories: '.fs-table-text_4',
+        loses: '.fs-table-text_5',
+        scored: '.fs-table-text_6',
+        conceded: '.fs-table-text_7',
+        cardsYellow: '.fs-table-text_8',
+        cardsRed: '.fs-table-text_9'
+    }
 
-    console.log({
-        rawTeam,
-        rawVistories,
-        rawLoses,
-        rawScored,
-        rawConceded,
-        rawCardsYellow,
-        rawCardsRed
-    });
-})
+    const cleanText = text => text
+        .replace(/\t|\n|\s:/g, '')
+        .replace(/.*:/g, ' ')
 
-const leaderboard = [{
-    team: 'Team 1',
-    wins: 0,
-    loses: 0,
-    goalsScored: 0,
-    goalsConceded: 0,
-    cardsYellow: 0,
-    cardsRed: 0,
-}]
+    const leaderBoardSelectEntries = Object.entries(LEADERBOARD_SELECTORS)
+
+    $rows.each((index, elem) => {
+        const leaderBoardEntries = Object.entries(LEADERBOARD_SELECTORS).map(([key, selector]) => {
+            const rawValue = $(elem).find(selector).text()
+            const value = cleanText(rawValue)
+            return [key, value]
+        })
+        Object.fromEntries(leaderBoardEntries)
+    })
+}
+
+getLeaderBoard()
