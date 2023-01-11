1c7843ac7e346124e70aef31429d2c2ade56cf70
Jose Sissa <jsissame3484@universidadean.edu.co>
2023-01-10 22:22:52 -0500

    Root for statics logos in API

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-	return c.json([
-		{
-			endpoint: '/leaderboard',
-			description: 'Return the leaderboard'
-		}
-	])
-})
-app.get('/leaderboard', (c) => {
-	return c.json(leaderboard)
-})
+app.get('/', (c) => c.json([
+	{
+		endpoint: '/leaderboard',
+		description: 'Return the leaderboard'
+	}
+]))
+
+app.get('/leaderboard', (c) =>  c.json(leaderboard))
+
+app.get('/static/*', serveStatic({ root: './' }))
+
 
 export default app
